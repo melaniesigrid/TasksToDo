48b5eab08f694b6c81bf7e229480e69e72681474
Melanie Arellano <melaniesigrid@protonmail.com>
2022-01-18 09:33:44 -0600

    Change docs back to dist

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   devServer: {
-    static: './docs',
+    static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -14,7 +14,7 @@ module.exports = {
   ],
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'docs'),
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   module: {
